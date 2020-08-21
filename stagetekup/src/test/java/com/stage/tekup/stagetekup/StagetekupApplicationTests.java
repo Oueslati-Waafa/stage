11a1879e6915aaa02b661497b5bd69089b900be9
Oueslati-Waafa <63404321+Oueslati-Waafa@users.noreply.github.com>
2020-08-21 17:37:50 +0100

    adding dependencies

---
+++ stagetekup/src/test/java/com/stage/tekup/stagetekup/StagetekupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stage.tekup.stagetekup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StagetekupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
